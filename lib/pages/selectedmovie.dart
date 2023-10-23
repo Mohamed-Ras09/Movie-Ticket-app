590ffb4e35f77deb24bf440e291f456b0d1f86b3
Mohamed Raasith <raasithknr@gmail.com>
2023-10-23 09:17:17 +0530

    late push

---
+++ lib/pages/selectedmovie.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class SelectedMovie extends StatelessWidget {
+  const SelectedMovie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
